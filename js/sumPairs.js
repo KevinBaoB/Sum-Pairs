16096bc50b5523521f4e2f066f46197185b55d65
KevinBaoB <kevinbaobui@gmail.com>
2022-05-26 14:37:09 -0500

    Second

--- js/sumPairs.js
+++ js/sumPairs.js
@@ -1,24 +1,34 @@
-// exports.sumPairs =
-
-function sumPairs(arr, target) {
+const sumPairs = function (arr, target) {
   let res = [];
-  for (let i in arr) {
-    for (let j in arr) {
-      if (target - arr[i] === arr[j]) {
-        res.push([arr[i], arr[j]]);
-      }
-    }
-  }
-  for (let k = 0; k < res.length; k++) {
-    for (let h = 0; h < res.length; h++) {
-      if (res[h].reverse() === res[k]) {
-        res.pop(res[h]);
-      }
+  for (let i = 0; i < arr.length; i++) {
+    if (arr.indexOf(target - arr[i], i + 1) > 0) {
+      res.push([arr[i], target - arr[i]]);
     }
   }
   if (res.length === 0) return "unable to find pairs";
   return res;
-}
+};
+
+// const sumPairs = function (arr, target) {
+//   let res = [];
+//   for (let i in arr) {
+//     for (let j in arr) {
+//       if (target - arr[i] === arr[j]) {
+//         res.push([arr[i], arr[j]]);
+//         // console.log(res);
+//       }
+//     }
+//   }
+//   for (let k = 0; k < res.length; k++) {
+//     for (let h = 0; h < res.length; h++) {
+//       if (res[h].reverse() === res[k]) {
+//         res.pop(res[h]);
+//       }
+//     }
+//   }
+//   if (res.length === 0) return "unable to find pairs";
+//   return res;
+// };
 
 console.log(sumPairs([1, 2, 3, 4, 5], 9));
 console.log(sumPairs([1, 2, 3, 4, 5], 7));
--- js/sumPairsSpec.js
+++ js/sumPairsSpec.js
@@ -1,3 +1,12 @@
 var sp = require("./sumPairs");
+var shallowEqualArrays = require("shallow-equal/Arrays");
+
+console.log(shallowEqualArrays(sp.sumPairs([1, 2, 3, 4, 5], 9), [[4, 5]]));
+console.log(
+  shallowEqualArrays(sp.sumPairs([1, 2, 3, 4, 5], 7), [
+    [2, 5],
+    [3, 4],
+  ])
+);
 
 // Don't forget to add your tests :)
